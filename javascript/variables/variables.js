6d0a5a8294de1168423f875c82eab3af0a83da28
Miguel Ramos <return.infox@gmail.com>
2023-10-20 20:09:43 -0300

    codigo javascript

---
+++ javascript/variables/variables.js
@@ -0,0 +1,18 @@
+var precio;
+var precio;
+precio = 200;
+console.log(precio);
+precio = "fernanda";
+console.log(precio);
+total = precio * 2;
+
+//* SUPER SET -> TYPESCRIPT
+
+//* ES6 2015
+
+//* let y const 
+let saldo;
+
+//* const
+const PI = 3.14;
+PI = 20;
